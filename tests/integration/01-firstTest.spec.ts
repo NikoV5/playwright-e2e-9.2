18a55fb775708f35efa4fda832d4be8182cdee46
TechGlobalSchool <tgschool01@gmail.com>
2024-07-21 11:11:45 -0500

    first commit

---
+++ tests/integration/01-firstTest.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("First Test Sute", () => {
+  test("Refrest, navigate back and forward", async ({ page }) => {
+    // Navigate to a page
+    await page.goto("https://techglobal-training.com/");
+
+    // Refresh the page
+    await page.reload();
+
+    // Navigate to another page
+    await page.goto("https://techglobal-training.com/frontend");
+
+    // Navigate back
+    await page.goBack();
+
+    // Navigate forward
+    await page.goForward();
+  });
+
+  test("Validate page title", async ({ page }) => {
+    await page.goto("https://techglobal-training.com/");
+
+    // const title = await page.title();
+
+    // console.log(title, " My page title");
+
+    // expect(title).toBe('TechGlobal Training | Home')
+
+    await expect(page).toHaveTitle('TechGlobal Training | Home')
+  });
+
+  test("Validate page URL", async ({ page }) => {
+    await page.goto("https://techglobal-training.com/");
+
+    // const url = page.url()
+
+    // expect(url).toBe('https://techglobal-training.com/')
+
+    await expect(page).toHaveURL('https://www.techglobal-training.com/')
+  });
+});
